0e7bfb9399f2a2a9c41ba4c7bcca0bec0163ebfe
Nimesh Deepamal <nimeshdeepamal@gmail.com>
2025-10-28 16:16:36 +0530

    Update Task entity

--- lib/domain/entities/task.dart
+++ lib/domain/entities/task.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/foundation.dart';
-
-@immutable
 class Task {
   final String id;
   final String title;
@@ -8,17 +5,23 @@ class Task {
   final DateTime dueDate;
   final bool completed;
 
-  const Task({
+  Task({
     required this.id,
     required this.title,
     required this.description,
     required this.dueDate,
-    this.completed = false,
+    required this.completed,
   });
 
-  Task copyWith({String? title, String? description, DateTime? dueDate, bool? completed}) {
+  Task copyWith({
+    String? id,
+    String? title,
+    String? description,
+    DateTime? dueDate,
+    bool? completed,
+  }) {
     return Task(
-      id: id,
+      id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
       dueDate: dueDate ?? this.dueDate,
