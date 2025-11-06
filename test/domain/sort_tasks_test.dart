3c9505471d0721ab91c9af20947b5ab77587111e
Nimesh Deepamal <nimeshdeepamal@gmail.com>
2025-11-06 12:09:41 +0530

    Add unit test: sort tasks ascending by due date

--- test/domain/sort_tasks_test.dart
+++ test/domain/sort_tasks_test.dart
@@ -5,20 +5,26 @@ void main() {
   test('Tasks sort ascending by due date', () {
     final t1 = Task(
       id: '1',
-      title: '1',
-      description: '',
+      title: 'Task 1',
+      description: 'First task',
       dueDate: DateTime(2025, 1, 2),
-      completed: false, 
+      completed: false,
+      userId: 'user1',
     );
+
     final t2 = Task(
       id: '2',
-      title: '2',
-      description: '',
+      title: 'Task 2',
+      description: 'Second task',
       dueDate: DateTime(2025, 1, 1),
-      completed: false, 
+      completed: false,
+      userId: 'user1',
     );
+
     final list = [t1, t2];
     list.sort((a, b) => a.dueDate.compareTo(b.dueDate));
+
     expect(list[0].id, '2');
+    expect(list[1].id, '1');
   });
 }
