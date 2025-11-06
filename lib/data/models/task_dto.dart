5dba9ad473de0c6a1f2519a10a8d5c5c0b57fc6a
Nimesh Deepamal <nimeshdeepamal@gmail.com>
2025-11-06 12:04:56 +0530

    feat(task): add userId to TaskDto

--- lib/data/models/task_dto.dart
+++ lib/data/models/task_dto.dart
@@ -1,4 +1,4 @@
-
+import 'package:todo_app/domain/entities/task.dart';
 
 class TaskDto {
   final String id;
@@ -6,6 +6,7 @@ class TaskDto {
   final String description;
   final int dueDate;
   final int completed;
+  final String userId;
 
   TaskDto({
     required this.id,
@@ -13,6 +14,7 @@ class TaskDto {
     required this.description,
     required this.dueDate,
     required this.completed,
+    required this.userId,
   });
 
   Map<String, dynamic> toMap() {
@@ -22,16 +24,34 @@ class TaskDto {
       'description': description,
       'dueDate': dueDate,
       'completed': completed,
+      'userId': userId,
     };
   }
 
-  factory TaskDto.fromMap(Map<String, dynamic> map) {
-    return TaskDto(
-      id: map['id'] as String,
-      title: map['title'] as String,
-      description: map['description'] as String,
-      dueDate: map['dueDate'] as int,
-      completed: map['completed'] as int,
-    );
-  }
+  factory TaskDto.fromMap(Map<String, dynamic> map) => TaskDto(
+        id: map['id'] as String,
+        title: map['title'] as String,
+        description: map['description'] as String,
+        dueDate: map['dueDate'] as int,
+        completed: map['completed'] as int,
+        userId: map['userId'] as String,
+      );
+
+  factory TaskDto.fromEntity(Task task) => TaskDto(
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        dueDate: task.dueDate.millisecondsSinceEpoch,
+        completed: task.completed ? 1 : 0,
+        userId: task.userId,
+      );
+
+  Task toEntity() => Task(
+        id: id,
+        title: title,
+        description: description,
+        dueDate: DateTime.fromMillisecondsSinceEpoch(dueDate),
+        completed: completed == 1,
+        userId: userId,
+      );
 }
